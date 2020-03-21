3d3df10f4f061b4c281f6719ab5f3a20198b5af0
Lucas Kjaero <lucas@lucaskjaero.com>
2020-03-21 15:42:29 -0700

    Handle new required fields

--- content/src/main/scala/CEDICT.scala
+++ content/src/main/scala/CEDICT.scala
@@ -1,7 +1,7 @@
 import org.apache.spark.sql.SparkSession
 import org.elasticsearch.spark.sql._
 
-case class CEDICTDefinition(subdefinitions: Array[String], simplified: String, traditional: String, pinyin: String)
+case class CEDICTDefinition(subdefinitions: Array[String], simplified: String, traditional: String, pinyin: String, language: String, token: String, source: String)
 
 object CEDICT {
   def main(args: Array[String]) {
@@ -11,10 +11,10 @@ object CEDICT {
     import spark.implicits._
 
     val cedict = spark
-        .read.textFile("cedict_ts.u8").cache()
-        // Strip out the long header with license and FAQ
-        .filter(_.startsWith("#"))
-        .map(line => parseLine(line))
+      .read.textFile("cedict_ts.u8").cache()
+      // Strip out the long header with license and FAQ
+      .filter(_.startsWith("#"))
+      .map(line => parseLine(line))
 
 
     cedict.saveToEs("cedict")
@@ -36,6 +36,6 @@ object CEDICT {
     val characterSection = cutAtPinyinFront(0).stripSuffix(" ").split(' ')
     val definitionSection = cutAtPinyinBack(1).stripPrefix(" ").stripPrefix("/").stripSuffix("/")
 
-    CEDICTDefinition(traditional=characterSection(0), simplified=characterSection(1), pinyin=cutAtPinyinBack(0), subdefinitions=definitionSection.split('/'))
+    CEDICTDefinition(traditional = characterSection(0), simplified = characterSection(1), pinyin = cutAtPinyinBack(0), subdefinitions = definitionSection.split('/'), language = "CHINESE", token = characterSection(1), source = "CEDICT")
   }
 }
